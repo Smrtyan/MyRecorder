d7c5a558be883eb8aab9b52777eb913934ea0fb5
smrtyan <547505786@qq.com>
2018-11-10 10:45:19 +0800

    Now it can record ,play and pause audio.

--- app/src/main/java/com/example/test/myrecorder/PlayRecordDialog.java
+++ app/src/main/java/com/example/test/myrecorder/PlayRecordDialog.java
@@ -9,22 +9,36 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import java.io.File;
 
 public class PlayRecordDialog extends DialogFragment {
+    MediaPlayer mediaPlayer;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout_recorder_player, null);
         String savedName = getArguments().getString("savedName");
-        MediaPlayer mediaPlayer = MediaPlayer.create(getActivity(), Uri.parse("file://" + savedName));
+        mediaPlayer = MediaPlayer.create(getActivity(), Uri.parse("file://" + savedName));
         mediaPlayer.start();
+
         view.findViewById(R.id.btn_record_play).setOnClickListener(v->{
             //TODO to pause or resume
-            
+            Button button = (Button)v;
+            if(button.getText().toString().equals("PAUSE")){
+                button.setText("RESUME");
+                mediaPlayer.pause();
+            }else {
+                button.setText("PAUSE");
+                mediaPlayer.start();
+            }
+
         });
-        view.findViewById(R.id.btn_record_back).setOnClickListener(v -> {dismiss();});
+        view.findViewById(R.id.btn_record_back).setOnClickListener(v -> {
+            if(mediaPlayer!=null)
+                mediaPlayer.stop();
+            dismiss();});
         return view;
     }
 }
